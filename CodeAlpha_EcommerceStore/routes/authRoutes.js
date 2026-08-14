ef8034d856ade65671dc4040526d285908c2c08c
Nikhil Kumar Das <nikhil.das6986@gmail.com>
2026-08-15 00:49:40 +0530

    feat: add authentication routes

--- CodeAlpha_EcommerceStore/routes/authRoutes.js
+++ CodeAlpha_EcommerceStore/routes/authRoutes.js
@@ -9,4 +9,6 @@ router.post("/register",authController.register);
 router.get("/login",authController.showLoginPage);
 router.post("/login",authController.login);
 
+router.post("/logout",authController.logout)
+
 module.exports = router;
