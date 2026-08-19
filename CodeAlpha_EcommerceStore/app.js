aa675979073b7ff39accd94da32360b4826e8d5f
Nikhil Kumar Das <nikhil.das6986@gmail.com>
2026-08-19 21:50:58 +0530

    refactor: update application configuration

--- CodeAlpha_EcommerceStore/app.js
+++ CodeAlpha_EcommerceStore/app.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const session = require("express-session");
 const { MongoStore } = require("connect-mongo");
 const flash = require("connect-flash");
+const methodOverride = require("method-override");
 
 const connectDB = require("./config/db");
 
@@ -16,34 +17,55 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
-// Import Routes
-// const indexRoutes = require("./routes/index");
+// IMPORT ROUTES
+
 const authRoutes = require("./routes/authRoutes");
-const testRoutes = require("./routes/testRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const checkoutRoutes = require("./routes/checkoutRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
-app.use(express.urlencoded({ extended: true })); //takes that incoming data and turns it into a JavaScript object:
 
+// MIDDLEWARE
+
+// Parse incoming form data
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
+
+// Session
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+
     store: MongoStore.create({
       mongoUrl: process.env.MONGO_URI,
     }),
   }),
 );
- 
+
+// Flash messages
 app.use(flash());
 
-// Use Routes
-// app.use("/", indexRoutes);
+// ROUTES
+
 app.use("/", authRoutes);
-app.use("/", testRoutes);
+app.use("/admin", adminRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+app.use("/checkout", checkoutRoutes);
+app.use("/orders", orderRoutes);
+app.use("/profile", profileRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to CodeAlpha Ecommerce Store!");
 });
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
